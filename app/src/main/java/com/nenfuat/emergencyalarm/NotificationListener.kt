0017c9a91799ce71f2fff9d38c14ac3a94c1b0a1
k21091 <k21091kk@aitech.ac.jp>
2024-05-13 13:59:06 +0900

    fix:不要なインポートを削除

--- app/src/main/java/com/nenfuat/emergencyalarm/NotificationListener.kt
+++ app/src/main/java/com/nenfuat/emergencyalarm/NotificationListener.kt
@@ -3,9 +3,6 @@ package com.nenfuat.emergencyalarm
 import android.app.*
 import android.content.Intent
 import android.media.MediaPlayer
-import android.os.Build
-import android.os.IBinder
-import android.provider.Settings
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
